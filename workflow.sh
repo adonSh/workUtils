7b924e7d7d69ef7bb43887acd74f72e268fdc2ee
Adon Shapiro <adon.shapiro@gmail.com>
2021-03-07 21:43:22 -0600

    added extensible workflow setup script

---
+++ workflow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Workflow functions ##
+demo()
+{
+  tmux new-session -d -s demo
+  tmux send-keys -t demo:1 'echo "This is a demonstration"' Enter
+  tmux attach-session -t demo
+}
+
+## Interface ##
+[[ -z $1 ]] && read -p "Enter workflow (? for help): " workflow || workflow=$1
+
+if [[ $workflow = ? ]]; then
+  echo Options:
+  declare -F | sed 's/declare -f/ /'
+else
+  $workflow 2> /dev/null || echo "Unkown workflow: \"$workflow\""
+fi
+
