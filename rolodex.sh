b9da207c5dd43f5b166fbcc38b0668b0f9184fa2
Adon Shapiro <adon.shapiro@gmail.com>
2022-06-29 11:54:24 -0500

    Made small adjustments to rolodex script

--- rolodex.sh
+++ rolodex.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-new() {
-  printf '%-8s %s\n' "$1" "$2"
-}
-
 fname="$HOME/.rolodex"
 
 if [ "$1" = add ]; then
   [ -z "$2" ] && read -p "name: " name || name="$2"
   read -p "number: " number
   new "$name" "$number" >> "$fname"
+  printf '%-8s %s\n' "$name" "$number" >> "$fname"
+elif [ "$1" = edit ]; then
+  vim "$fname"
 elif [ -z "$1" ]; then
   cat "$fname"
 else
